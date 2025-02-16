82e4e7390f03a62aeb3f7a0c99a4c770bca376cc
kazrin <kazunori.yoshimura1988@gmail.com>
2025-02-17 06:47:57 +0900

    Add togglePin firmware

---
+++ src/subscribeAttributes/togglePin.cpp
@@ -0,0 +1,100 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "config.h"
+#include "utils.h"
+
+class PinState
+{
+private:
+    bool state;
+    int pinNumber;
+    String pinNumberStr;
+
+public:
+    PinState(int pinNumber) : pinNumber(pinNumber),
+                              pinNumberStr(String(pinNumber))
+    {
+        state = false;
+        pinMode(pinNumber, OUTPUT);
+    }
+
+    String stringify()
+    {
+        JsonDocument doc;
+        doc["pinState"] = state;
+        String output;
+        serializeJson(doc, output);
+        return output;
+    }
+
+    void toggle()
+    {
+        digitalWrite(pinNumber, !state);
+        state = !state;
+    }
+
+    void setState(bool state)
+    {
+        digitalWrite(pinNumber, state);
+        this->state = state;
+    }
+};
+
+PinState pinState(32);
+
+WiFiClient wifiClient;
+PubSubClient pubsubClient(wifiClient);
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    String incoming = "";
+    for (int i = 0; i < length; i++)
+    {
+        incoming += (char)payload[i];
+    }
+    Serial.println("Message arrived [" + String(topic) + "] " + incoming);
+
+    JsonDocument doc;
+    deserializeJson(doc, incoming);
+
+    // Update pin state
+    pinState.setState(doc["pinState"]);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    // Connect to WiFi
+    WiFi.begin(WiFiConfig::ssid, WiFiConfig::password);
+    if (!connectToWiFi())
+    {
+        ESP.restart();
+    }
+
+    pubsubClient.setServer(MQTTBrokerConfig::server, MQTTBrokerConfig::port);
+    pubsubClient.setCallback(callback);
+}
+
+void loop()
+{
+    if (pubsubClient.connected())
+    {
+        pubsubClient.loop();
+    }
+    else
+    {
+        Serial.println("Connecting to MQTT broker...");
+        bool connected = pubsubClient.connect(
+            BasicMQTT::client_id,
+            BasicMQTT::user_name,
+            BasicMQTT::password);
+        if (connected)
+        {
+            pubsubClient.subscribe("v1/devices/me/attributes");
+        }
+    }
+
+    delay(1);
+}
